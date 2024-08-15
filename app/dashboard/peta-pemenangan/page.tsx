e1cf597abee38fdb5cc5f4c9b38e0769e2a033ad
Aldi <aldifayol@gmail.com>
2024-08-15 21:23:15 +0700

    update dependencies

--- app/dashboard/peta-pemenangan/page.tsx
+++ app/dashboard/peta-pemenangan/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function PetaPemenangan() {
-  return <p>Peta Pemenangan Chloropeth</p>;
+// let map = L.map("map").setView([37.8, -96], 4);
+
+function PetaPemenangan() {
+  return (
+    <>
+      <div id="map"></div>
+      <p>Peta Pemenangan</p>;
+    </>
+  );
 }
+
+export default PetaPemenangan;
